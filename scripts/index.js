6fd3c06cb7f48e5c810739376a53afb836edc322
sakibmahbub <arafatsakib48@gmail.com>
2023-03-01 23:28:54 +0600

    displayed category count and name on alert section

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,22 @@ const fetchCategories = async () => {
 };
 
 const showCategories = (data) => {
-  console.log(data);
   const categoryContainer = document.getElementById("categories-container");
   data.news_category.forEach((category) => {
-    categoryContainer.innerHTML += ` <a class="nav-link" href="#">${category.category_name}</a>`;
+    categoryContainer.innerHTML += ` <a class="nav-link" href="#" onclick="fetchCategoryNews('${category.category_id}','${category.category_name}')">${category.category_name}</a>`;
   });
+};
+
+const fetchCategoryNews = async (category_id, category_name) => {
+  // console.log(category_id);
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  showAllNews(data, category_name);
+};
+
+const showAllNews = (data, category_name) => {
+  console.log(data, category_name);
+  document.getElementById("category-count").innerText = data.data.length;
+  document.getElementById("category-name").innerText = category_name;
 };
