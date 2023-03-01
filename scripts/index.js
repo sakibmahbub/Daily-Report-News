7ac9af872b30591eb41511506e445d4e9e0e1125
sakibmahbub <arafatsakib48@gmail.com>
2023-03-02 01:04:32 +0600

    fetched the api and displayed news title,image & description

--- scripts/index.js
+++ scripts/index.js
@@ -21,7 +21,31 @@ const fetchCategoryNews = async (category_id, category_name) => {
 };
 
 const showAllNews = (data, category_name) => {
-  console.log(data, category_name);
   document.getElementById("category-count").innerText = data.length;
   document.getElementById("category-name").innerText = category_name;
+  const newsContainer = document.getElementById("all-news");
+  newsContainer.innerHTML = "";
+  data.forEach((news) => {
+    console.log(news);
+    newsContainer.innerHTML += `
+    <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${news.image_url}" class="img-fluid rounded-start"/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text">
+              ${news.details.slice(0, 200)}...
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+  });
 };
